d4e70203bf7da3725f562f1dd79f71a22781b52c
Bisht <g.bisht@accenture.com>
2020-01-16 15:52:42 +0530

    Kubernetes ingress controller changes

--- src/app/shared/service/users.service.ts
+++ src/app/shared/service/users.service.ts
@@ -9,16 +9,22 @@ export class UsersService {
   constructor(private httpClient: HttpClient) { }
 
   public getUsers() {
-    return this.httpClient.get('http://' + window.location.host + '/details/');
+    return this.httpClient.get('/details/');
+
+    // return this.httpClient.get('http://' + window.location.host + '/details/');
   }
 
   // https://jsonplaceholder.typicode.com/users
 
   public getUserDetails(id: String) {
-    return this.httpClient.get('http://' + window.location.host + '/details/' + id);
+    return this.httpClient.get('/details/' + id);
+    
+    // return this.httpClient.get('http://' + window.location.host + '/details/' + id);
   }
 
   public getReviews(id: String) {
-    return this.httpClient.get('http://' + window.location.host + '/reviews/' + id);
+    return this.httpClient.get('/reviews/' + id);
+
+    // return this.httpClient.get('http://' + window.location.host + '/reviews/' + id);
   }
 }
